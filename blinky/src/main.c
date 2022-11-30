6b66afe2c10a06db27528fd06a2156b312d990af
JohnnySn0w <33181572+JohnnySn0w@users.noreply.github.com>
2022-11-30 16:22:47 -0600

    Add button interrupt add timer

--- blinky/src/main.c
+++ blinky/src/main.c
@@ -11,6 +11,7 @@
 
 
 #define SLEEPTIME				2000
+#define AMBIENT_COUNTDOWN		30
 #define VCNL4040_I2C_ADDR		0x60
 #define VCNL4040_H_THRESH_REG	0x01
 #define VCNL4040_H_THRESH_H		0x75 //30000
@@ -27,6 +28,7 @@
 #define INT_PIN_CONFIG			(GPIO_INPUT|GPIO_ACTIVE_HIGH|GPIO_INT_ENABLE|GPIO_PULL_UP)
 // #define I2C_NODE				DT_NODELABEL(vcnl40401)
 // #define GPIO_NODE				DT_NODELABEL(gpio1)
+#define BUTTON3					DT_NODELABEL(button3)
 
 typedef struct bufMsg{
 	int regAddr; 
@@ -43,6 +45,7 @@ bool vcnlWrite(const struct device* dev, uint8_t* i2c_buffer, bufMsg* msg);
 bool vcnlRead(const struct device* dev, uint8_t* i2c_buffer, uint8_t reg);
 void int_triggered(const struct device* dev, struct gpio_callback* callb, uint32_t pin);
 bool sensorSetup(const struct device* dev, uint8_t* i2c_buffer);
+void proxyBLE(const struct device *gpioButton, struct gpio_callback *callback, gpio_port_pins_t pins);
 bool setupGPIO(const struct device* gp_cont);
 void calibrate(const struct device* dev, uint8_t* buf, calVals* calibVals);
 void pollALS(const struct device* dev, uint8_t* i2c_buffer);
@@ -59,8 +62,12 @@ void pollALS(const struct device* dev, uint8_t* i2c_buffer);
 // };
 
 //global flag
-int INTflag = 0;
-int freshCalib = 1;
+bool INTflag = false;
+bool freshCalib = true;
+bool buttonFlag = false;
+bool sleepFlag = false;
+static const struct gpio_dt_spec button3_spec = GPIO_DT_SPEC_GET(BUTTON3, gpios);
+struct k_timer ambientTimer;
 
 bool vcnlWrite(const struct device* dev, uint8_t* i2c_buffer, bufMsg* msg){
 	i2c_buffer[0] = msg->regAddr;
@@ -92,17 +99,14 @@ bool vcnlRead(const struct device* dev, uint8_t* i2c_buffer, uint8_t reg)
 
 void int_triggered(const struct device* dev, struct gpio_callback* callb, uint32_t pin)
 {
-	INTflag=1;
+	INTflag = true;
+	sleepFlag = false;
 }
 
-// bool setINTReg(const struct dev, uint8_t* i2c_buffer, )
-// {
-// 	msg.regAddr = VCNL4040_H_THRESH_REG;
-// 	msg.LByte = VCNL4040_H_THRESH_L;
-// 	msg.HByte = VCNL4040_H_THRESH_H;
-// 	if(!vcnlWrite(dev, i2c_buffer, &msg))
-// 	{return false;}
-// }
+void proxyBLE(const struct device *gpioButton, struct gpio_callback *callback, gpio_port_pins_t pins)
+{
+	buttonFlag = !buttonFlag;
+}
 
 bool sensorSetup(const struct device* dev, uint8_t* i2c_buffer) 
 {
@@ -148,7 +152,12 @@ bool setupGPIO(const struct device* gp_cont){
 	{
 		ret = gpio_pin_configure(gp_cont, INT_PIN, INT_PIN_CONFIG);
 		if(ret != 0){ 
-			printk("Error %d: failed to configure pin", ret);
+			printk("Error %d: failed to configure INT pin", ret);
+			return false;
+		}
+		ret = gpio_pin_configure_dt(&button3_spec, GPIO_INPUT);
+		if(ret != 0){ 
+			printk("Error %d: failed to configure button pin", ret);
 			return false;
 		}
 	}
@@ -158,24 +167,51 @@ bool setupGPIO(const struct device* gp_cont){
 		printk("Error %d: failed to configure interrupt for pin", ret);
 		return false;
 	} 
+	ret = gpio_pin_interrupt_configure_dt(&button3_spec, GPIO_INT_EDGE_TO_ACTIVE);
+	if(ret != 0) 
+	{
+		printk("Error %d: failed to configure interrupt for pin", ret);
+		return false;
+	}
+	//int pin setup
 	static struct gpio_callback interrupt_callback_data;
 	gpio_init_callback(&interrupt_callback_data, int_triggered, BIT(INT_PIN));
 	gpio_add_callback(gp_cont, &interrupt_callback_data);
+	//button setup
+	static struct gpio_callback button3_pressed;
+	gpio_init_callback(&button3_pressed, proxyBLE, BIT(button3_spec.pin));
+	gpio_add_callback(button3_spec.port, &button3_pressed);
 	return true;
 }
 
 void calibrate(const struct device* dev, uint8_t* buf, calVals* calibVals)
 {
+	static uint16_t aggre;
+	static int reads;
+	static int avg;
+
 	uint16_t reading = ((uint16_t)(buf[1])<<8) + (uint16_t)(buf[0]);
 	bufMsg msg;
-	if(freshCalib == 1)
+	if(freshCalib == true)
 	{
+		aggre = reading;
+		reads = 1; 
 		calibVals->ambH = reading+1;
 		calibVals->ambL = reading-1;
-		freshCalib = 0;
+		freshCalib = false;
 		printk("updated ambH to %u\n", calibVals->ambH);
 		printk("updated ambL to %u\n", calibVals->ambL);
 		return;
+	} else {
+		aggre += reading;
+		reads += 1;
+		if (sleepFlag | reads > 20)
+		{
+			avg = aggre/reads;
+			aggre = avg;
+			reads = 1;
+		}
+		printk("aggre is %d. avg is %d. ", aggre, avg);
 	}
 	if(reading > calibVals->ambH)
 	{
@@ -203,17 +239,28 @@ void pollALS(const struct device* dev, uint8_t* i2c_buffer)
 {
 	//poll ALS
 	calVals calibValues; //store highest and lowest ambients
-	while(vcnlRead(dev, i2c_buffer, VCNL4040_ALS_REG))
-	{
+	vcnlRead(dev, i2c_buffer, VCNL4040_ALS_REG);
 		calibrate(dev, i2c_buffer, &calibValues);
 		if(INTflag == 1)
 		{
 			printk("interrupt was triggered\n");
 			vcnlRead(dev, i2c_buffer, VCNL4040_INT_FLAG);
 			INTflag = 0;
+		printk("looking for bluetooth...\n");
+		k_msleep(1000);
 		}
-		k_msleep(SLEEPTIME);
+	printk("bluetooth %s!\n", buttonFlag ? "on" : "off");
 	}
+
+void sleepFuncCb(struct k_timer *dummy)
+{
+	sleepFlag = true; 
+	//put device into sleep mode, waiting for interrupt
+}
+
+void startTimer()
+{
+	k_timer_start(&ambientTimer, K_SECONDS(AMBIENT_COUNTDOWN),K_SECONDS(0));
 }
 
 void main(void)
@@ -234,6 +281,17 @@ void main(void)
 		printk("GPIO setup failed.");
 		return;
 	}
-
+	//create and start minute countdown
+	k_timer_init(&ambientTimer, sleepFuncCb, NULL);
+	startTimer();
+	vcnlRead(dev, i2c_buffer, VCNL4040_INT_FLAG);
+	while(true)
+	{
+		if(!sleepFlag)
+		{
 	pollALS(dev, i2c_buffer);
+			k_msleep(SLEEPTIME);
+		}
+		k_msleep(1);
+	}
 }
